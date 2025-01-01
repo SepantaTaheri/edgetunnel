494cc85253f0d9de47c8a0593bcac2f83eb13a84
Sepanta Taheri <87647244+SepantaTaheri@users.noreply.github.com>
2025-01-01 13:45:22 +0330

    Update 明文源码.js

--- 明文源码.js
+++ 明文源码.js
@@ -1,9 +1,3 @@
-/*!
-  * v2ray Subscription Worker v2.3
-  * Copyright 2024 Vahid Farid (https://twitter.com/vahidfarid)
-  * Licensed under GPLv3 (https://github.com/vfarid/v2ray-worker/blob/main/Licence.md)
-  */
-
 var __create = Object.create;
 var __defProp = Object.defineProperty;
 var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
@@ -8303,27 +8297,3 @@ var worker_default = {
 export {
   worker_default as default
 };
-/*! Bundled license information:
-
-bcryptjs/dist/bcrypt.js:
-  (**
-   * @license bcrypt.js (c) 2013 Daniel Wirtz <dcode@dcode.io>
-   * Released under the Apache License, Version 2.0
-   * see: https://github.com/dcodeIO/bcrypt.js for details
-   *)
-
-ieee754/index.js:
-  (*! ieee754. BSD-3-Clause License. Feross Aboukhadijeh <https://feross.org/opensource> *)
-
-buffer/index.js:
-  (*!
-   * The buffer module from node.js, for the browser.
-   *
-   * @author   Feross Aboukhadijeh <https://feross.org>
-   * @license  MIT
-   *)
-
-js-yaml/dist/js-yaml.mjs:
-  (*! js-yaml 4.1.0 https://github.com/nodeca/js-yaml @license MIT *)
-*/
-//# sourceMappingURL=worker.js.map
